a48c3e4fff858eb3856734e07eff076e17cb0b0f
ClaireSenga <marieclairemangue2908@gmail.com>
2024-05-16 06:14:25 -0700

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -54,3 +54,26 @@ class BaseModel:
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return (new_dict)
+
+    def __init__(self, *args, **kwargs):
+        """a recreation of an instance with dictionary representation"""
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+
