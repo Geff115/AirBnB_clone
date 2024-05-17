b40776b192452febc05abcece2ad195fad5705d8
ClaireSenga <marieclairemangue2908@gmail.com>
2024-05-17 00:11:14 -0700

    updated base model

--- models/base_model.py
+++ models/base_model.py
@@ -18,17 +18,32 @@ ARGS:
 
 from uuid import uuid4
 from datetime import datetime
-
+import json
+from os import path
 
 class BaseModel:
     """The BaseModel class"""
 
-    def __init__(self):
-        """Initialization method"""
+     def __init__(self, *args, **kwargs):
+        """a recreation of an instance with dictionary representation"""
 
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """str method that returns a string representation of the object"""
@@ -55,25 +70,35 @@ class BaseModel:
         new_dict['updated_at'] = self.updated_at.isoformat()
         return (new_dict)
 
-    def __init__(self, *args, **kwargs):
-        """a recreation of an instance with dictionary representation"""
+class FileStorage:
+    """link of BaseModel to Filestorage"""
+    __file_path = "file.json"
+    __objects = {}
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+    def all(self):
+        """returns the dictionary of all objects"""
+        return FileStorage.__objects
 
+    def new(self, obj):
+        """new method that adds a new object to the __objects dictionary"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
+    def save(self):
+        """this method serializes all objects to a JSON file"""
+        serialized_objects = {}
+        for key, obj in FileStorage.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(FileStorage.__file_path, "w") as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        """the reload method deserializes the JSON file to objects if the file exists"""
+        if path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as file:
+                loaded_objects = json.load(file)
+                for key, value in loaded_objects.items():
+                    class_name, obj_id = key.split('.')
+                    class_obj = eval(class_name)
+                    obj = class_obj(**value)
+                    FileStorage.__objects[key] = obj
