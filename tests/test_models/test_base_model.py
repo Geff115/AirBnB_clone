e03cd1cf236e1600b58d65d9fe274c2210cb2111
gabrielnoah129@gmail.com <Geff115>
2024-05-18 06:57:41 +0100

    Modified the BaseModel to handle invalid datetime format, handling *args and **kwargs, and passed all unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,10 +6,12 @@ the datetime module.
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
+from uuid import uuid4
 
 
 class TestBaseModel(unittest.TestCase):
     """Runs a series of unittest on the BaseModel class"""
+
 
     def test_initialization(self):
         """Runs a test on the init method"""
@@ -48,6 +50,42 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(obj_dict['__class__'], obj.__class__.__name__)
         self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
         self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
+
+    def test_kwargs_initialization(self):
+        "Runs a test on the keyworded arguments initialization"""
+
+        data = {
+            'id' : '1234',
+            'created_at': '2023-01-01T12:00:00.000000',
+            'updated_at': '2023-01-01T12:00:00.000000',
+            'name': 'Test'
+        }
+        obj = BaseModel(**data)
+        self.assertEqual(obj.id, '1234')
+        self.assertEqual(obj.created_at, datetime.fromisoformat('2023-01-01T12:00:00.000000'))
+        self.assertEqual(obj.updated_at, datetime.fromisoformat('2023-01-01T12:00:00.000000'))
+        self.assertEqual(obj.name, 'Test')
+
+    def test_args_initialization(self):
+        """Runs a test to ensure *args are not used"""
+
+        obj = BaseModel('arg1', 'arg2')
+        self.assertIsInstance(obj, BaseModel)
+        self.assertNotIn('arg1', obj.__dict__)
+        self.assertNotIn('arg2', obj.__dict__)
+
+    def test_invalid_daytime_format(self):
+        """Runs a test to check invalid daytime format"""
+
+        invalid_data = {
+                'created_at': 'invalid_data',
+                'updated_at': 'invalid_data'
+        }
+        obj = BaseModel(**invalid_data)
+        self.assertIsInstance(obj.created_at, datetime)
+        self.assertIsInstance(obj.updated_at, datetime)
+        self.assertNotEqual(obj.created_at.isoformat(), 'invalid_date')
+        self.assertNotEqual(obj.updated_at.isoformat(), 'invalid_date')
 
 
 if __name__ == '__main__':
