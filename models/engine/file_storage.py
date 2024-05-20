1af005576d2a2902813c90cfe263212cfd9fb8f7
gabrielnoah129@gmail.com <Geff115>
2024-05-20 04:45:42 +0100

    Updated FileStorage to manage serialization and deserializaton of all our new classes, I update the console to allow these commands

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,6 +25,11 @@ import os
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from model.review import Review
 
 
 class FileStorage:
